0a8a21b8fc41e54821076a7b2cfadd4e2ccf36b5
Guillaume Hain <zedtux@zedroot.org>
2016-11-25 06:35:00 +0100

    Keeps the column title out of the scroll

--- app/assets/javascripts/sento/kanban/kanban.js
+++ app/assets/javascripts/sento/kanban/kanban.js
@@ -1,6 +1,6 @@
 $(document).on('ready page:load', function() {
   $('.sento-kanban').perfectScrollbar();
-  $('.sento-kanban-column').perfectScrollbar();
+  $('.sento-kanban-cards').perfectScrollbar();
 
   dragula($('.sento-kanban-cards').toArray(), {
     copySortSource: true

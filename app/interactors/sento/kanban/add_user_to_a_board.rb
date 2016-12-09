28030c2923c0359dae22da6e010081a9b23a4e0d
Guillaume Hain <zedtux@zedroot.org>
2016-12-09 05:58:26 +0100

    Removes debug messages

--- app/interactors/sento/kanban/add_user_to_a_board.rb
+++ app/interactors/sento/kanban/add_user_to_a_board.rb
@@ -4,15 +4,11 @@ module Sento
       include Interactor
 
       def call
-        Rails.logger.debug "[debug(#{__FILE__.split("app/")[1]}:#{__LINE__})] AddUserToABoard.call ..."
         board = context.board
         user = context.user
 
-        Rails.logger.debug "[debug(#{__FILE__.split("app/")[1]}:#{__LINE__})] board: #{board.inspect}"
-        Rails.logger.debug "[debug(#{__FILE__.split("app/")[1]}:#{__LINE__})] user: #{user.inspect}"
         context.fail! unless board && user
 
-        Rails.logger.debug "[debug(#{__FILE__.split("app/")[1]}:#{__LINE__})] Adding user to the board ..."
         board.users << user
       end
     end

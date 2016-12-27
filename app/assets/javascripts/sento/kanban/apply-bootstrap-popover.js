64f9a78e2a2510813d449cfc54921e643db46d49
Guillaume Hain <zedtux@zedroot.org>
2016-12-27 14:40:53 +0100

    Improves the card members popover * Fixes multiple hooking of the popover javascript function * Fixes the check icon style

--- app/assets/javascripts/sento/kanban/apply-bootstrap-popover.js
+++ app/assets/javascripts/sento/kanban/apply-bootstrap-popover.js
@@ -1,17 +1,22 @@
 function applyBootstrapPopover() {
   $('*[data-toggle="popover"]').bind('click', function(event){
     event.preventDefault();
-    var self = $(this),
-        url = self.attr('href');
 
-    $.get(url, function(data) {
-      self.popover({
-        content: data
-      }).popover('show');
+    if ($(this).data('bootstrap-popover') !== 'undefined') {
+      var self = $(this),
+          url = self.attr('href');
 
-      $('#' + self.attr('aria-describedby') + ' button[aria-label="Close"]').on('click', function() {
-        $('#' + self.attr('aria-describedby')).popover('hide');
+      $(this).data('bootstrap-popover', 'hooked');
+
+      $.get(url, function(data) {
+        self.popover({
+          content: data
+        }).popover('show');
+
+        $('#' + self.attr('aria-describedby') + ' button[aria-label="Close"]').on('click', function() {
+          $('#' + self.attr('aria-describedby')).popover('hide');
+        });
       });
-    });
+    }
   });
 }

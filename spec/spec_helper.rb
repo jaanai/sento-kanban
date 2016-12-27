bbe8e21c7a3bdd8bbb778d0b7829f85b21c23481
Guillaume Hain <zedtux@zedroot.org>
2016-12-27 18:14:30 +0100

    Fixes the ElasticSearch instance in test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
 #
+
+ENV['ELASTICSEARCH_URL'] = 'http://searchengine:9201'
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
